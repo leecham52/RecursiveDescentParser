3a22da14aaf3b28a0421e4d3129e5f4eb7f78286
leecham52 <lunatic52@naver.com>
2023-11-06 00:50:40 +0900

    오류 수정

--- lex.cpp
+++ lex.cpp
@@ -4,20 +4,30 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <stack>
 #include <ctype.h>
 #include <stdio.h>
 #include <string.h>
 using namespace std;
 
 //아쉽지만, 전역변수로 받기
-//vector형으로 바꾸기
 vector<vector<int>> tokens;
 vector<vector<string>> lexeme;
 int fl = 0;
 int f = 0;
-string* dup_op;
-int s = 0;
-int* cur_ident;
+vector<string> dup_op;
+string error;
+struct id {
+    string id;
+    int val;
+};
+struct op {
+    string op;
+    int p;
+};
+vector<id> ide;
+stack<int> num;
+stack<op> oper;
 
 #define IDENT 10
 #define CONST 11
@@ -27,6 +37,7 @@ int* cur_ident;
 #define MUL_OP 16
 #define LPAREN 18
 #define RPAREN 19
+#define UNKNOWN -10000
 
 bool checkWhiteSpace(char c);
 bool checkLetter(char c);
@@ -59,28 +70,28 @@ void left_paren(vector<vector<int>> tks);
 void right_paren(vector<vector<int>> tks);
 
 void lexical(string str);
+void calString();
+void calIdent(vector<vector<int>> tk, vector<vector<string>> lex, vector<id>& ident);
 void printResult(vector<vector<int>> tk, vector<vector<string>> lex);
 
 
 int main(int argc, char* argv[]) {
     
-    /*if (argc < 2)
-	{
-		cout << "파일 입력 오류" << endl;
-		return 0;
-	}*/
     string in;
-    
+
     //c++에 맞춰서 변환, string으로 낑겨 받으면 좋을수도 아닐수도
-    ifstream in_Fp(argv[1]);
-    if (!in_Fp.is_open())
-        cout<< "ERROR - cannot open front.in" << endl;
-    while(!in_Fp.eof()){
+    ifstream in_Fp("input1.txt");
+    if (!in_Fp.is_open()) {
+        cout << "ERROR - cannot open front.in" << endl;
+        return 0;
+    }
+    while (!in_Fp.eof()) {
         getline(in_Fp, in); //한 줄씩 입력 받음
         lexical(in);
     }
     in_Fp.close();
     program(tokens); //RecursiveDescentParser 하기
+    calIdent(tokens, lexeme, ide);
     printResult(tokens, lexeme);
 
     return 0;
@@ -165,67 +176,168 @@ void lexical(string str) {
             n_t.push_back(next_token);
         }
     }
+    //2차원 vector에 저장
     tokens.push_back(n_t);
     lexeme.push_back(lex);
 }
+void calString() {
+    int a, b, result;
+    b = num.top();
+    num.pop();
+    a = num.top();
+    num.pop();
+    string operate = oper.top().op;
+    oper.pop();
+    if (operate == "*")
+        result = a * b;
+    else if (operate == "/")
+        result = a / b;
+    else if (operate == "+")
+        result = a + b;
+    else if (operate == "-")
+        result = a - b;
+    num.push(result);
+}
+void calIdent(vector<vector<int>> tk, vector<vector<string>> lex, vector<id>& ident) {
+    //ident 계산
+    for (int k = 0; k < tk.size(); k++) {
+        for (int i = 2; i < tk[k].size(); i++) {
+            if (tk[k][i] == IDENT) {
+                for (int j = 0; j < ident.size(); j++) {
+                    int l = 0;
+                    if (lex[k][i] == ident[j].id) {
+                        num.push(ident[j].val);
+                    }
+                    else {
+                        l++;
+                        if (l == ident.size()) {
+                            num.push(UNKNOWN);
+                            ident.push_back({lex[k][i], UNKNOWN});
+                            error = to_string(k) + lex[k][i];
+                        }
+                    }
+                }
+            }
+            else if (tk[k][i] == CONST) {
+                num.push(stoi(lex[k][i]));
+            }
+            else if (tk[k][i] == ADD_OP || tk[k][i] == MUL_OP) {
+                int pr = 3;
+                if (lex[k][i] == "*" || lex[k][i] == "/") {
+                    pr = 2;
+                }
+                else if (lex[k][i] == "+" || lex[k][i] == "-") {
+                    pr = 1;
+                }
+                while (!oper.empty() && pr <= oper.top().p) {
+                    calString();
+                }
+                oper.push({ lex[k][i], pr });
+            }
+            else if (tk[k][i] == LPAREN) {
+                oper.push({lex[k][i], 0});
+            }
+            else if (tk[k][i] == RPAREN) {
+                while (oper.top().op != "(") {
+                    calString();
+                }
+                oper.pop();
+            }
+        }
+        while (!oper.empty()) {
+            calString();
+        }
+        if (tk[k][0] == IDENT) {
+            ident.push_back({ lex[k][0], num.top() });
+            num.pop();
+        }
+    }
+}
 //출력, ID, CONST, OP의 개수를 나타내주는 함수
-void printResult(int** tk, string** lex){
-    for(int k = 0; k< sizeof(tk)/sizeof(tk[0]);k++){
+void printResult(vector<vector<int>> tk, vector<vector<string>> lex) {
+    for (int k = 0; k < tk.size(); k++) {
         int id_count = 0;
         int const_count = 0;
         int op_count = 0;
         string m;
         m.clear();
-        for(int i =0; i< sizeof(tk[k])/sizeof(int); i++){
-            
+        for (int i = 0; i < tk[k].size(); i++) {
             m += lex[k][i];
-            m += " ";
-            if(tk[k][i] == IDENT){
+            if (i != tk[k].size() - 2) {
+                m += " ";
+            }
+            if (tk[k][i] == IDENT) {
                 id_count++;
             }
-            else if(tk[k][i] == CONST){
+            else if (tk[k][i] == CONST) {
                 const_count++;
             }
-            else if(tk[k][i] == ADD_OP || tk[k][i] == MUL_OP){
+            else if (tk[k][i] == ADD_OP || tk[k][i] == MUL_OP) {
                 op_count++;
             }
         }
-        cout<<m<<endl;
-        cout<<"ID: "<<id_count<<"; CONST: "<<const_count<<"; OP: "<<op_count<<";"<<endl;
+        cout << m << endl;
+        cout << "ID: " << id_count << "; CONST: " << const_count << "; OP: " << op_count << ";" << endl;
+        for (int s = 0; s < dup_op.size(); s++) {
+            if (dup_op[s].front() == (char)k) {
+                cout << "(WARNING) \"중복 연산자(" << dup_op[s].back() << ") 제거 \" " << endl;
+            }
+        }
+        if (!error.empty()  && error.front() == char(k)) {
+            error.erase(0);
+            cout << "(Error) \"정의되지 않은 변수("<< error << ")가 참조됨\"" << endl;
+        }
+        cout << "(OK)" << endl;
         //OK, WARNING, Error
-
     }
+    //Result 
+    cout << "Result ==> ";
+    for (int n = 0; n < ide.size(); n++) {
+        if (ide[n].val < 0) {
+            if (n == ide.size() - 1) {
+                cout << ide[n].id << ": Unknown";
+            }
+            cout << ide[n].id << ": " << "Unknown;";
+        }
+        else {
+            if (n == ide.size() - 1) {
+                cout << ide[n].id << ": " << to_string(ide[n].val);
+            }
+            cout << ide[n].id << ": " << to_string(ide[n].val) << ";";
+        }
+    }
+    
 }
 //ascii code 32이하인지 확인
-bool checkWhiteSpace(char c){
-    if(c <= 32){
+bool checkWhiteSpace(char c) {
+    if (c <= 32) {
         return true;
     }
     return false;
 }
 bool checkLetter(char c) {
-    if (c >= 65 && c <= 90) {  
-		return true;
-	}
-	else if (c >= 97 && c <= 122) { 
-		return true;
-	}
-	else return false;
+    if (c >= 65 && c <= 90) {
+        return true;
+    }
+    else if (c >= 97 && c <= 122) {
+        return true;
+    }
+    else return false;
 }
-bool checkDigit(char c){
-    if (c >= 48 && c <= 57) { 
-		return true;
-	}
-	else return false;
+bool checkDigit(char c) {
+    if (c >= 48 && c <= 57) {
+        return true;
+    }
+    else return false;
 }
 //identifier인지 확인
-bool checkIdent(const char c[]){
-    if(c[0] == '_' || checkLetter(c[0])){
-        if((int)strlen(c) == 1){ //한 자릿 수의 identifier일 때
+bool checkIdent(const char c[]) {
+    if (c[0] == '_' || checkLetter(c[0])) {
+        if ((int)strlen(c) == 1) { //한 자릿 수의 identifier일 때
             return true;
         }
-        for (int i = 1; i<(int)strlen(c); i++){
-            if(c[i] != '_' && !checkLetter(c[i]) && !checkDigit(c[i])){
+        for (int i = 1; i < (int)strlen(c); i++) {
+            if (c[i] != '_' && !checkLetter(c[i]) && !checkDigit(c[i])) {
                 return false;
             }
         }
@@ -234,16 +346,16 @@ bool checkIdent(const char c[]){
     else return false;
 }
 //const인지 확인
-bool checkConst(const char c[]){
-    if(c[0] == '0' && (int)strlen(c) == 1){ //0일때
+bool checkConst(const char c[]) {
+    if (c[0] == '0' && (int)strlen(c) == 1) { //0일때
         return true;
     }
-    if(checkDigit(c[0]) && c[0] != '0'){
-        for(int i = 1; i<=(int)strlen(c); i++){
-            if(checkDigit(c[i])){
+    if (checkDigit(c[0]) && c[0] != '0') {
+        for (int i = 1; i <= (int)strlen(c); i++) {
+            if (checkDigit(c[i])) {
                 continue;
             }
-            else if(c[i] == '\0') return true;
+            else if (c[i] == '\0') return true;
             else return false;
         }
     }
@@ -252,142 +364,142 @@ bool checkConst(const char c[]){
 //+,-인지 확인 + ++, --에 대해서는? 따로 처리 레츠고
 //++, -- 한번씩만 더 쓴 걸로 생각 하고 +-, -+ 같은 경우는 false로 처리
 //
-bool checkAddOp(const char c[]){
-    if((int)strlen(c) == 2){
-        if((c[0] == '+' && c[1] == '+') || (c[0] == '-' && c[1] == '-')){
+bool checkAddOp(const char c[]) {
+    if ((int)strlen(c) == 2) {
+        if ((c[0] == '+' && c[1] == '+') || (c[0] == '-' && c[1] == '-')) {
             return true;
         }
     }
-    if((int)strlen(c) != 1){
+    if ((int)strlen(c) != 1 || (int)strlen(c) != 2) {
         return false;
     }
-    if(c[0] == '+' || c[0] == '-'){
+    if (c[0] == '+' || c[0] == '-') {
         return true;
     }
     return false;
 }
 //*,/인지 확인
-bool checkMulOp(const char c[]){
-    if((int)strlen(c) == 2){
-        if((c[0] == '*' && c[1] == '*') || (c[0] == '/' && c[1] == '/')){
-            
+bool checkMulOp(const char c[]) {
+    if ((int)strlen(c) == 2) {
+        if ((c[0] == '*' && c[1] == '*') || (c[0] == '/' && c[1] == '/')) {
+            return true;
         }
     }
-    if((int)strlen(c) != 1){
+    if ((int)strlen(c) != 1 || (int)strlen(c) != 2) {
         return false;
     }
-    if(c[0] == '*' || c[0] == '/'){
+    if (c[0] == '*' || c[0] == '/') {
         return true;
     }
     return false;
 }
 // := 인지 확인
-bool checkAssignOp(const char c[]){
-    if((int)strlen(c) == 2 && c[0] == ':' && c[1] == '='){
+bool checkAssignOp(const char c[]) {
+    if ((int)strlen(c) == 2 && c[0] == ':' && c[1] == '=') {
         return true;
-    } 
+    }
     return false;
 }
 //(인지 확인
-bool checkLparen(const char c[]){
-    if((int)strlen(c) == 1 && c[0] == '('){
+bool checkLparen(const char c[]) {
+    if ((int)strlen(c) == 1 && c[0] == '(') {
         return true;
     }
     return false;
 }
 //)인지 확인
-bool checkRparen(const char c[]){
-    if((int)strlen(c) == 1 && c[0] == ')'){
+bool checkRparen(const char c[]) {
+    if ((int)strlen(c) == 1 && c[0] == ')') {
         return true;
     }
     return false;
 }
 //;인지 확인
-bool checkSemiColon(const char c[]){
-    if((int)strlen(c) == 1 && c[0] == ';'){
+bool checkSemiColon(const char c[]) {
+    if ((int)strlen(c) == 1 && c[0] == ';') {
         return true;
-    }   
+    }
     return false;
 }
-int checkToken(const char t[]){
+int checkToken(const char t[]) {
     if (checkIdent(t)) {
-		return IDENT;
-	}
+        return IDENT;
+    }
     else if (checkConst(t)) {
-		return CONST;
-	}
-	else if (checkAddOp(t)) {
-		return ADD_OP;
-	}
+        return CONST;
+    }
+    else if (checkAddOp(t)) {
+        return ADD_OP;
+    }
     else if (checkMulOp(t)) {
-		return MUL_OP;
-	}
-	else if (checkAssignOp(t)) {
-		return ASSIGN_OP;
-	}
-	else if (checkLparen(t)) {
-		return LPAREN;
-	}
-	else if (checkRparen(t)) {
-		return RPAREN;
-	}
+        return MUL_OP;
+    }
+    else if (checkAssignOp(t)) {
+        return ASSIGN_OP;
+    }
+    else if (checkLparen(t)) {
+        return LPAREN;
+    }
+    else if (checkRparen(t)) {  
+        return RPAREN;
+    }
     else if (checkSemiColon(t)) {
-		return SEMI_COLON;
-	}
+        return SEMI_COLON;
+    }
     else return 0;
 }
 //Assignment 1 조건에 맞는 Recursive-Descent Parsing
 //<program> -> <statements>
 void program(vector<vector<int>> tks) {
-    cout << "Enter <program>" << endl;
+    //cout << "Enter <program>" << endl;
     statements(tks);
-    cout << "Exit <program>" << endl;
+    //cout << "Exit <program>" << endl;
 }
 //<statements> -> <statement> {<semi_colon><statements}
 void statements(vector<vector<int>> tks) {
-    cout << "Enter <statements>" << endl;
+    //cout << "Enter <statements>" << endl;
     statement(tks);
     if (tokens[fl][f] == SEMI_COLON) {
         semi_colon(tks);
         statements(tks);
     }
-    cout << "Exit <statements>" << endl;
+    //cout << "Exit <statements>" << endl;
 }
 //<statement> -> <ident> <assignment_op> <expression>
 void statement(vector<vector<int>> tks) {
-    cout << "Enter <statement>" << endl;
+    //cout << "Enter <statement>" << endl;
     ident(tks);
     assignment_op(tks);
     expression(tks);
-    cout << "Exit <statement>" << endl;
+    //cout << "Exit <statement>" << endl;
 }
 //<expression> -> <term> <term_tail>
 void expression(vector<vector<int>> tks) {
-    cout << "Enter <expression>" << endl;
+    //cout << "Enter <expression>" << endl;
     term(tks);
     term_tail(tks);
-    cout << "Exit <expression>" << endl;
+    //cout << "Exit <expression>" << endl;
 }
 //<term> -> <factor> <factor_tail>
 void term(vector<vector<int>> tks) {
-    cout << "Enter <term>" << endl;
+    //cout << "Enter <term>" << endl;
     factor(tks);
     factor_tail(tks);
-    cout << "Exit <term>" << endl;
+    //cout << "Exit <term>" << endl;
 }
 //<term_tail> -> {<add_op> <term> <term_tail>}
 void term_tail(vector<vector<int>> tks) {
     if (tks[fl][f] == ADD_OP) {
-        cout << "Enter <term_tail>" << endl;
+        //cout << "Enter <term_tail>" << endl;
         add_op(tks);
         term(tks);
         term_tail(tks);
-        cout << "Exit <term_tail>" << endl;
+        //cout << "Exit <term_tail>" << endl;
     }
 }
 //<factor> -> (<left_paren> <expression> <right_paren> | <ident> | <const>)
 void factor(vector<vector<int>> tks) {
-    cout << "Enter <factor>" << endl;
+    //cout << "Enter <factor>" << endl;
     if (tks[fl][f] == LPAREN) {
         left_paren(tks);
         expression(tks);
@@ -399,107 +511,103 @@ void factor(vector<vector<int>> tks) {
     else if (tks[fl][f] == CONST) {
         const_(tks);
     }
-    cout << "Exit <factor>" << endl;
+    //cout << "Exit <factor>" << endl;
 }
 //<factor_tail> -> {<mult_op> <factor> <factor_tail>}
 void factor_tail(vector<vector<int>> tks) {
     if (tks[fl][f] == MUL_OP) {
-        cout << "Enter <factor_tail>" << endl;
+        //cout << "Enter <factor_tail>" << endl;
         mult_op(tks);
         factor(tks);
         factor_tail(tks);
-        cout << "Exit <factor_tail>" << endl;
+        //cout << "Exit <factor_tail>" << endl;
     }
 }
 //<const> -> any decimal numbers
 void const_(vector<vector<int>> tks) {
-    cout << "Enter <const>" << endl;
+    //cout << "Enter <const>" << endl;
     if (tks[fl][f] == CONST) {
-        if (!(fl == sizeof(tks) / sizeof(tks[fl]) - 1)) {
+        if (fl != tks.size()-1 || f != tks[fl].size() - 1) {
             f++;
         }
     }
-    cout << "Exit <const>" << endl;
+    //cout << "Exit <const>" << endl;
 }
 //<ident> -> any names conforming to C id rules
 void ident(vector<vector<int>> tks) {
-    cout << "Enter <ident>" << endl;
+    //cout << "Enter <ident>" << endl;
     if (tks[fl][f] == IDENT) {
-        if (!(fl == sizeof(tks) / sizeof(tks[fl]) - 1)) {
+        if (fl != tks.size() - 1 || f != tks[fl].size()-1) {
             f++;
         }
     }
-    cout << "Exit <ident>" << endl;
+    //cout << "Exit <ident>" << endl;
 }
 //<assignment_op> -> :=
 void assignment_op(vector<vector<int>> tks) {
-    cout << "Enter <assignment_op>" << endl;
+    //cout << "Enter <assignment_op>" << endl;
     if (tks[fl][f] == ASSIGN_OP) {
         f++;
     }
-    cout << "Exit <assignmnet_op>" << endl;
+    //cout << "Exit <assignmnet_op>" << endl;
 }
 //<semi_colon> -> ;
 //여기서 줄이 바뀐다, 2차원 배열이 바뀌는 것,
 void semi_colon(vector<vector<int>> tks) {
-    cout << "Enter <semi_colon>" << endl;
+    //cout << "Enter <semi_colon>" << endl;
     if (tks[fl][f] == SEMI_COLON) {
-        if (sizeof(tks[fl]) / sizeof(int) == f + 1) { // 배열 길이, 
+        if (tks[fl].size() == f + 1) { // 배열 길이, 
             fl++;
             f = 0;
         }
     }
-    cout << "Exit <semi_colon>" << endl;
+    //cout << "Exit <semi_colon>" << endl;
 }
 //<add_op> -> (+|-)
 void add_op(vector<vector<int>> tks) {
-    cout << "Enter <add_op>" << endl;
+    //cout << "Enter <add_op>" << endl;
     if (lexeme[fl][f] == "++") {
         lexeme[fl][f] = "+";
-        dup_op[s] = to_string(fl) + "+";
-        s++;
+        dup_op.push_back(char(fl) + lexeme[fl][f]);
     }
     else if (lexeme[fl][f] == "--") {
         lexeme[fl][f] = "-";
-        dup_op[s] = to_string(fl) + "-";
-        s++;
+        dup_op.push_back(char(fl) + lexeme[fl][f]);
     }
     if (tks[fl][f] == ADD_OP) {
         f++;
     }
-    cout << "Exit <add_op>" << endl;
+    //cout << "Exit <add_op>" << endl;
 }
 //<mult_op> -> (*|/)
 void mult_op(vector<vector<int>> tks) {
-    cout << "Enter <mult_op>" << endl;
+    //cout << "Enter <mult_op>" << endl;
     if (lexeme[fl][f] == "**") {
         lexeme[fl][f] = "*";
-        dup_op[s] = to_string(fl) + "*";
-        s++;
+        dup_op.push_back(char(fl) + lexeme[fl][f]);
     }
     else if (lexeme[fl][f] == "//") {
         lexeme[fl][f] = "/";
-        dup_op[s] = to_string(fl) + "/"; //몇 번줄에서 일어났는지,
-        s++;
+        dup_op.push_back(char(fl) + lexeme[fl][f]);
     }
     if (tks[fl][f] == MUL_OP) {
         f++;
     }
-    cout << "Exit <mult_op>" << endl;
+    //cout << "Exit <mult_op>" << endl;
 }
 //<left_paren> -> (
 void left_paren(vector<vector<int>> tks) {
-    cout << "Enter <left_paren>" << endl;
+    //cout << "Enter <left_paren>" << endl;
     if (tks[fl][f] == LPAREN) {
         f++;
     }
-    cout << "Exit <left_paren>" << endl;
+    //cout << "Exit <left_paren>" << endl;
 }
 //<right_paren> -> )
 void right_paren(vector<vector<int>> tks) {
-    cout << "Enter <right_paren>" << endl;
+    //cout << "Enter <right_paren>" << endl;
     if (tks[fl][f] == RPAREN) {
         f++;
     }
-    cout << "Exit <right_paren>" << endl;
+    //cout << "Exit <right_paren>" << endl;
 }
